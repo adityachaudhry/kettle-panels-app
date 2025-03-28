8b3b3d6646cd4b750a1dc5c4b694315c7ab71475
adchaudh <chaudhry.aditya1@gmail.com>
2025-03-28 16:31:01 -0700

    tailwind works

--- src/app.tsx
+++ src/app.tsx
@@ -2,4 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
-root.render(<div className="flex items-center justify-center h-screen"></div>);
+root.render(
+  <div className="flex flex-col h-screen">
+    <div
+      className="h-[3.2rem] flex-shrink-0 border-b"
+      style={{ WebkitAppRegion: "drag" } as React.CSSProperties}
+    />
+    <div className="flex-1 items-center justify-center">Hello World</div>
+  </div>
+);
--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,7 @@ const createWindow = (): void => {
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
+    trafficLightPosition: { x: 20, y: 16 },
   });
 
   // and load the index.html of the app.
