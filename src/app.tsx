cb61e984e59b4e5e5a9e8bed5f17c2d575a46b63
adchaudh <chaudhry.aditya1@gmail.com>
2025-03-29 21:59:24 -0700

    layout

--- src/app.tsx
+++ src/app.tsx
@@ -5,9 +5,19 @@ const root = createRoot(document.body);
 root.render(
   <div className="flex flex-col h-screen">
     <div
-      className="h-[3.2rem] flex-shrink-0 border-b"
+      className="h-[3.2rem] flex-shrink-0 flex"
       style={{ WebkitAppRegion: "drag" } as React.CSSProperties}
-    />
-    <div className="flex-1 items-center justify-center">Hello World</div>
+    >
+      <div className="w-[160px] border-r"></div>
+      <div className="flex-1"></div>
+    </div>
+    <div className="flex flex-1">
+      <div className="w-[160px] flex-shrink-0 border-r">
+        {/* Sidebar content */}
+      </div>
+      <div className="flex-1 p-4 flex items-center justify-center border-t">
+        <h1 className="text-2xl font-bold">Hello World</h1>
+      </div>
+    </div>
   </div>
 );
