c1dccc69c289ae91acabedf02bd95dcff6f50f5c
adchaudh <chaudhry.aditya1@gmail.com>
2025-03-31 00:12:08 -0700

    minor

--- src/app.tsx
+++ src/app.tsx
@@ -10,11 +10,11 @@ const App: React.FC = () => {
         className="h-[3.2rem] flex-shrink-0 flex"
         style={{ WebkitAppRegion: "drag" } as React.CSSProperties}
       >
-        <div className="w-[160px] border-r dark:border-[#3F3F3F] border-[#D1D1D1]"></div>
+        <div className="w-[160px] border-r dark:border-black border-[#D1D1D1]"></div>
         <div className="flex-1 bg-[#F0F0F0] dark:bg-[#2F2F2F]"></div>
       </div>
       <div className="flex flex-1">
-        <div className="w-[160px] flex-shrink-0 border-r dark:border-[#3F3F3F] border-[#D1D1D1]">
+        <div className="w-[160px] flex-shrink-0 border-r dark:border-black border-[#D1D1D1]">
           {/* Sidebar content */}
         </div>
         <div className="flex-1 p-4 flex items-center justify-center bg-[#F0F0F0] dark:bg-[#2F2F2F] border-[#E5E5E5] dark:border-[#0F0F0F]">
