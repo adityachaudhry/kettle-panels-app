77e57d79656760a5de6b179b37c7865ec24cf90b
adchaudh <chaudhry.aditya1@gmail.com>
2025-04-24 00:00:19 -0400

    create and delete handled

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,32 +1,62 @@
 import React from "react";
 import FilePicker from "./FilePicker";
 import PhotoGrid from "./PhotoGrid";
+import { v4 as uuidv4 } from "uuid";
+
+interface PhotoItem {
+  url: string;
+  guid: string;
+}
 
 const Main: React.FC = () => {
-  const [photoURLs, setPhotoURLs] = React.useState<string[]>([]);
+  const [photos, setPhotos] = React.useState<PhotoItem[]>([]);
 
   const handleFilesSelect = (files: File[]) => {
     const readers = files.map((file) => {
-      return new Promise<string>((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = () => resolve(reader.result as string);
-        reader.onerror = reject;
-        reader.readAsDataURL(file);
-      });
+      return new Promise<{ url: string; name: string; guid: string }>(
+        (resolve, reject) => {
+          const reader = new FileReader();
+          const guid = uuidv4();
+          reader.onload = () =>
+            resolve({ url: reader.result as string, name: file.name, guid });
+          reader.onerror = reject;
+          reader.readAsDataURL(file);
+        }
+      );
     });
-    Promise.all(readers).then((urls) => {
-      setPhotoURLs((prev) => [...prev, ...urls]);
+    Promise.all(readers).then(async (fileObjs) => {
+      setPhotos((prev) => [
+        ...prev,
+        ...fileObjs.map((f) => ({ url: f.url, guid: f.guid })),
+      ]);
+      // Save to userData/kettle-panels/photos via main process
+      if (window.electronAPI?.invoke) {
+        await window.electronAPI.invoke("save-photos", {
+          files: fileObjs.map((f) => ({
+            guid: f.guid,
+            name: f.name,
+            dataUrl: f.url,
+          })),
+        });
+      }
     });
   };
 
-  const handleDeletePhoto = (index: number) => {
-    setPhotoURLs((prev) => prev.filter((_, i) => i !== index));
+  const handleDeletePhoto = async (index: number) => {
+    const photo = photos[index];
+    setPhotos((prev) => prev.filter((_, i) => i !== index));
+    if (window.electronAPI?.invoke) {
+      await window.electronAPI.invoke("delete-photo", { guid: photo.guid });
+    }
   };
 
   return (
     <div className="flex-1 p-4 flex flex-col items-center justify-center bg-[#F0F0F0] dark:bg-[#2F2F2F] border-[#E5E5E5] dark:border-[#0F0F0F]">
       <FilePicker onFilesSelect={handleFilesSelect} />
-      <PhotoGrid photos={photoURLs} onDelete={handleDeletePhoto} />
+      <PhotoGrid
+        photos={photos.map((p) => p.url)}
+        onDelete={handleDeletePhoto}
+      />
       {/* Main content */}
     </div>
   );
